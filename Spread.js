fd8007b00738fbecac92e480710f07f684aa1b77
HafidZayyan <hafidzayyan21@gmail.com>
2024-11-04 20:45:01 +0700

    belajar menggunakan map di js.

---
+++ Spread.js
@@ -0,0 +1,13 @@
+const obj1 = { name: 'Dicoding' };
+const obj2 = { lastName: 'Indonesia', address: 'Jl. Batik Kumeli No 50' };
+const newObj = { ...obj1, ...obj2 };
+
+console.log(newObj); // Output: { name: 'Dicoding', lastName: 'Indonesia', address: 'Jl. Batik Kumeli No 50' }
+
+
+// Spread juga bia=sa digunakan untuk menggabungkan array.
+const array1 = ['Dicoding'];
+const array2 = ['Indonesia', 'Jl. Batik Kumeli No 50'];
+const newArray = [...array1, ...array2];
+
+console.log(newArray); // Output: ['Dicoding', 'Indonesia', 'Jl. Batik Kumeli No 50']
---
+++ map.js
@@ -0,0 +1,12 @@
+const productMap = new Map([
+    ['shoes', 500],
+    ['cap', 350],
+    ['jeans', 250]
+  ]);
+  
+  console.log(productMap);
+
+  const map = new Map();
+map.set('name', 'aras');
+console.log(map); // Map(1) { 'name' => 'aras' }
+
