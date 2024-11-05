ebad1f478449739c90763f6e0c9ec83979faed88
HafidZayyan <hafidzayyan21@gmail.com>
2024-11-05 16:34:28 +0700

    belajar For loop di js.

---
+++ ForLoop.js
@@ -0,0 +1,7 @@
+  //digunakan untuk prorgram perulangan sederhana.
+
+for (let i = 0; i < 5; i++) {
+    console.log(`Angka ke-${i} adalah ${i}`);
+  }
+
+  //variabel i adalah 0 dan i kurang dari 5 maka jika (i++) maka i akan di tambahkan menjadi plus 1 sampai mencapai kondisi kurang dari 5. yaitu 0,1,2,3,4.
