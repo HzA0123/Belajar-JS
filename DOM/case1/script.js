5f7d1e6f573b51af9ec060a4855322206cac5c58
HafidZayyan <hafidzayyan21@gmail.com>
2025-03-10 09:47:57 +0700

    Menambahkan code ke script.js dan index.html

---
+++ DOM/case1/script.js
@@ -0,0 +1,17 @@
+const title = document.getElementById('title');
+console.log ('getElementById:', title )
+
+const paragraph = document.getElementsByTagName('p');
+console.log ('getElemntByTagName:',paragraph)
+
+const firstParagraf = document.querySelector('.text')
+console.log("first Paragraph", firstParagraf)
+
+const AllParagraf = document.querySelectorAll('.text')
+console.log("first Paragraph", AllParagraf)
+
+const button = document.getElementById('btn');
+
+
+
+
